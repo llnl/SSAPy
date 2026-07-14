9bcbc1b43b3ac9e1c26548c4c412118afc135f18
Travis Yeager <SuperdoerTrav@gmail.com>
2026-07-14 13:43:21 -0700

    Bump version to 1.1.6

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='ssapy',
-    version='1.1.5',
+    version='1.1.6',
     ext_modules=[CMakeExtension("ssapy._ssapy")],
     cmdclass={"build_ext": CMakeBuild},
     packages=find_packages(),
